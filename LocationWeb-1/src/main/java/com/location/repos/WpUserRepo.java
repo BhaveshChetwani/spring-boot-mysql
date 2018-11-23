33f781f67ede67a31ec9f4379c398bf5721bf11d
BhaveshChetwani <bhaveshchetwani@gmail.com>
2018-11-23 09:49:50 +0530

    Delete WpUserRepo.java

--- LocationWeb-1/src/main/java/com/location/repos/WpUserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.location.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.location.entities.WpUser;
-
-public interface WpUserRepo extends JpaRepository<WpUser, Integer> {
-
-}
